1ade2cff869906f0c2384c62674e888990147f6f
tetradog <tetradog@gmail.com>
2017-03-12 23:27:31 +0100

    testing la nueva version del framework :D

--- AtaquesTesting/MainWindow.xaml.cs
+++ AtaquesTesting/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace AtaquesTesting
 	public partial class MainWindow : Window
 	{
         RomData romData;
+        Ataque[] ataques;
         Ataque ataqueActual;
         public MainWindow()
 		{
@@ -54,8 +55,8 @@ namespace AtaquesTesting
             if (romData!=null)
             if (MessageBox.Show("Guardar Cambios?", "Atención", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes)
             {
-                romData.SetRomData();
-                romData.RomGBA.Guardar();
+            	Ataque.SetAtaques(romData,ataques);
+                romData.Rom.Save();
             }
         }
 
@@ -70,16 +71,16 @@ namespace AtaquesTesting
 			if (opnFile.ShowDialog().GetValueOrDefault()) {
 
 				romData = new RomData(opnFile.FileName);
+				ataques=Ataque.GetAtaques(romData);
+				cmbTipos.ItemsSource = Tipo.GetTipos(romData); 
 
-                cmbTipos.ItemsSource = romData.Tipos;
-
-                romData.Pokedex[0].OrdenPokedexNacional = 0;//pongo bien a missigno :D
-                romData.Pokedex.Sort();
+               // romData.Pokedex[0].OrdenPokedexNacional = 0;//pongo bien a missigno :D
+                //romData.Pokedex.Sort();
 
                 lstAtaques.Items.Clear();
-                for (int i = 1; i < romData.Ataques.Count; i++)
+                for (int i = 1; i < ataques.Length; i++)
                 {
-                    lstAtaques.Items.Add(romData.Ataques[i]);
+                    lstAtaques.Items.Add(ataques[i]);
 
                 }
                 lstAtaques.SelectedIndex = 0;
@@ -100,30 +101,30 @@ namespace AtaquesTesting
                 GuardarCambiosHechos();
                 ataqueActual = ataqueSeleecionado;
                 //pongo los datos
-                indexAtaque =romData.Ataques.IndexOf(ataqueSeleecionado)-1;
-                cmbTipos.SelectedIndex = ataqueSeleecionado.DatosAtaque.Type;
+                indexAtaque =ataques.IndexOf(ataqueSeleecionado)-1;
+                cmbTipos.SelectedIndex = ataqueSeleecionado.Datos.Type;
 
-                txtPrioridad.Text = ataqueSeleecionado.DatosAtaque.Priority + "";
-                txtEfecto.Text = ataqueSeleecionado.DatosAtaque.Effect + "";
-                txtExactitud.Text = ataqueSeleecionado.DatosAtaque.Accuracy + "";
-                txtPrecisionExactitud.Text = ataqueSeleecionado.DatosAtaque.EffectAccuracy + "";
-                txtPp.Text = ataqueSeleecionado.DatosAtaque.PP + "";
-                txtPoderBase.Text = ataqueSeleecionado.DatosAtaque.BasePower + "";
-                txtTarget.Text = ataqueSeleecionado.DatosAtaque.Target + "";
+                txtPrioridad.Text = ataqueSeleecionado.Datos.Priority + "";
+                txtEfecto.Text = ataqueSeleecionado.Datos.Effect + "";
+                txtExactitud.Text = ataqueSeleecionado.Datos.Accuracy + "";
+                txtPrecisionExactitud.Text = ataqueSeleecionado.Datos.EffectAccuracy + "";
+                txtPp.Text = ataqueSeleecionado.Datos.PP + "";
+                txtPoderBase.Text = ataqueSeleecionado.Datos.BasePower + "";
+                txtTarget.Text = ataqueSeleecionado.Datos.Target + "";
 
                 txtDescripcion.Text = ataqueSeleecionado.Descripcion;
                 txtNombre.Text = ataqueSeleecionado.Nombre;
 
-                chkIsAffectedByKingsRock.IsChecked = ataqueSeleecionado.DatosAtaque.IsAffectedByKingsRock;
-                chkIsAffectedByMagicCoat.IsChecked = ataqueSeleecionado.DatosAtaque.IsAffectedByMagicCoat;
-                chkIsAffectedByMirrorWave.IsChecked = ataqueSeleecionado.DatosAtaque.IsAffectedByMirrorMove;
-                chkIsAffectedByProtect.IsChecked = ataqueSeleecionado.DatosAtaque.IsAffectedByProtect;
-                chkIsAffectedBySnatch.IsChecked = ataqueSeleecionado.DatosAtaque.IsAffectedBySnatch;
-                chkMakeContact.IsChecked = ataqueSeleecionado.DatosAtaque.MakesContact;
+                chkIsAffectedByKingsRock.IsChecked = ataqueSeleecionado.Datos.IsAffectedByKingsRock;
+                chkIsAffectedByMagicCoat.IsChecked = ataqueSeleecionado.Datos.IsAffectedByMagicCoat;
+                chkIsAffectedByMirrorWave.IsChecked = ataqueSeleecionado.Datos.IsAffectedByMirrorMove;
+                chkIsAffectedByProtect.IsChecked = ataqueSeleecionado.Datos.IsAffectedByProtect;
+                chkIsAffectedBySnatch.IsChecked = ataqueSeleecionado.Datos.IsAffectedBySnatch;
+                chkMakeContact.IsChecked = ataqueSeleecionado.Datos.MakesContact;
 
                 tapDatosAtaque.Header = "Datos ataque: " + ataqueSeleecionado.Nombre;
 
-                uniPokemonQueUsanElAtaque.Children.Clear();
+               /* uniPokemonQueUsanElAtaque.Children.Clear();
                 for (int i = 0; i < romData.Pokedex.Count; i++)
                 {
                     if (romData.Pokedex[i].AtaquesAprendidos.EstaElAtaque(indexAtaque))
@@ -133,7 +134,7 @@ namespace AtaquesTesting
                         uniPokemonQueUsanElAtaque.Children.Add(imgPokemon);
                     }
                 }
-                tapPokemonQueLoUsan.Header = "Pokemon que lo aprenden: " + uniPokemonQueUsanElAtaque.Children.Count;
+                tapPokemonQueLoUsan.Header = "Pokemon que lo aprenden: " + uniPokemonQueUsanElAtaque.Children.Count;*/
             }
         }
 
@@ -141,25 +142,25 @@ namespace AtaquesTesting
         {
             if(ataqueActual!=null)
             {
-                ataqueActual.DatosAtaque.Priority=byte.Parse(txtPrioridad.Text);
-                ataqueActual.DatosAtaque.Type=(byte)cmbTipos.SelectedIndex;
+                ataqueActual.Datos.Priority=byte.Parse(txtPrioridad.Text);
+                ataqueActual.Datos.Type=(byte)cmbTipos.SelectedIndex;
 
-                ataqueActual.DatosAtaque.Effect=byte.Parse(txtEfecto.Text);
-                ataqueActual.DatosAtaque.Accuracy=byte.Parse(txtExactitud.Text);
-                ataqueActual.DatosAtaque.EffectAccuracy=byte.Parse(txtPrecisionExactitud.Text);
-                ataqueActual.DatosAtaque.PP=byte.Parse(txtPp.Text);
-                ataqueActual.DatosAtaque.BasePower=byte.Parse(txtPoderBase.Text);
-                ataqueActual.DatosAtaque.Target=byte.Parse(txtTarget.Text);
+                ataqueActual.Datos.Effect=byte.Parse(txtEfecto.Text);
+                ataqueActual.Datos.Accuracy=byte.Parse(txtExactitud.Text);
+                ataqueActual.Datos.EffectAccuracy=byte.Parse(txtPrecisionExactitud.Text);
+                ataqueActual.Datos.PP=byte.Parse(txtPp.Text);
+                ataqueActual.Datos.BasePower=byte.Parse(txtPoderBase.Text);
+                ataqueActual.Datos.Target=byte.Parse(txtTarget.Text);
 
                 ataqueActual.Descripcion.Texto= txtDescripcion.Text;
                 ataqueActual.Nombre.Texto = txtNombre.Text;
 
-                ataqueActual.DatosAtaque.IsAffectedByKingsRock= chkIsAffectedByKingsRock.IsChecked.GetValueOrDefault();
-                ataqueActual.DatosAtaque.IsAffectedByMagicCoat= chkIsAffectedByMagicCoat.IsChecked.GetValueOrDefault();
-                ataqueActual.DatosAtaque.IsAffectedByMirrorMove= chkIsAffectedByMirrorWave.IsChecked.GetValueOrDefault();
-                ataqueActual.DatosAtaque.IsAffectedByProtect= chkIsAffectedByProtect.IsChecked.GetValueOrDefault();
-                ataqueActual.DatosAtaque.IsAffectedBySnatch= chkIsAffectedBySnatch.IsChecked.GetValueOrDefault();
-                ataqueActual.DatosAtaque.MakesContact= chkMakeContact.IsChecked.GetValueOrDefault();
+                ataqueActual.Datos.IsAffectedByKingsRock= chkIsAffectedByKingsRock.IsChecked.GetValueOrDefault();
+                ataqueActual.Datos.IsAffectedByMagicCoat= chkIsAffectedByMagicCoat.IsChecked.GetValueOrDefault();
+                ataqueActual.Datos.IsAffectedByMirrorMove= chkIsAffectedByMirrorWave.IsChecked.GetValueOrDefault();
+                ataqueActual.Datos.IsAffectedByProtect= chkIsAffectedByProtect.IsChecked.GetValueOrDefault();
+                ataqueActual.Datos.IsAffectedBySnatch= chkIsAffectedBySnatch.IsChecked.GetValueOrDefault();
+                ataqueActual.Datos.MakesContact= chkMakeContact.IsChecked.GetValueOrDefault();
 
                 lstAtaques.Items.Refresh();
             }
